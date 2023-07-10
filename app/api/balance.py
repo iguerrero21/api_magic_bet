04c6e168dcae00afccf34fb02957310771b3f695
iguerrero <vermasalla@yahoo.com.ar>
2023-07-10 16:42:29 -0300

    Solucionado problema con  /api/balance/users

--- app/api/balance.py
+++ app/api/balance.py
@@ -2,9 +2,23 @@ from fastapi import APIRouter, Depends, HTTPException
 from app.database import db
 from app.database.models.user import User as DBUser
 from app.database.models.wallet import Wallet as DBWallet
-from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.orm import Session
 
 router = APIRouter()
+
+@router.get("/api/balance/users")
+async def get_users_balance(db: Session = Depends(db.get_db)):
+    wallets = db.query(DBWallet).all()
+    if not wallets:
+        raise HTTPException(status_code=404, detail="No se encontraron billeteras")
+    
+    users_balance = []
+
+    for wallet in wallets:
+        print(wallet.user_id)
+        users_balance.append({"user_id": wallet.user_id, "balance": wallet.balance})
+    
+    return {"users_balance": users_balance}
 
 @router.get("/api/balance/{user_id}")
 async def get_user_balance(user_id: int, db: Session = Depends(db.get_db)):
@@ -18,14 +32,3 @@ async def get_user_balance(user_id: int, db: Session = Depends(db.get_db)):
     
     return {"balance": wallet.balance}
 
-@router.get("/api/balance/users")
-async def get_users_balance(db: Session = Depends(db.get_db)):
-    wallets = db.query(DBWallet).all()
-    if not wallets:
-        raise HTTPException(status_code=404, detail="No se encontraron billeteras")
-
-    users_balance = []
-    for wallet in wallets:
-        users_balance.append({"user_id": wallet.user_id, "balance": wallet.balance})
-    
-    return {"users_balance": users_balance}
